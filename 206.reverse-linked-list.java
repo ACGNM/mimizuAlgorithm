ddbe8b6502455ca3b7dd0d21b26651cd67c7ac82
zgong <gong_zhihao_blade@yahoo.co.jp>
2024-07-14 19:51:44 +0900

    add solution

---
+++ 206.reverse-linked-list.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=206 lang=java
+ *
+ * [206] Reverse Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        // 双指针，一个指向前一个，一个指向当前
+        // 为了去掉环，前一个指针从null开始
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode tmp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = tmp;
+        }
+        return prev;
+    }
+}
+// @lc code=end
+
---
+++ 234.palindrome-linked-list.java
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode id=234 lang=java
+ *
+ * [234] Palindrome Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        // 找到中间点然后反转中间点之后的链表
+        // 然后分别从链表的一开始和反转后的链表做比较
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        if (fast != null) {
+            slow = slow.next;
+        }
+        slow = reverse(slow);
+        fast = head;
+        while(slow != null) {
+            if (slow.val != fast.val) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next;
+
+        }
+        return true;
+    }
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode tmp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = tmp;
+        }
+        return prev;
+    }
+}
+// @lc code=end
+
